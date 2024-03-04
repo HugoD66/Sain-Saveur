78b540f54594c55c1254347783d17c84b3e07fda
MHUG <maurane.hugon@gmail.com>
2024-03-04 15:39:25 +0100

    login et register

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/Register';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [isSigningUp, setIsSigningUp] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isSigningUp ? (
+        <LoginPage onRegisterClick={() => setIsSigningUp(false)} /> 
+      ) : (
+        <RegisterPage onLoginClick={() => setIsSigningUp(true)} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
