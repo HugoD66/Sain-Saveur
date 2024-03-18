ff1e9d442b99e76a0255295c526122dee4b89d66
HugoD66 <dessauw.hugo@gmail.com>
2024-03-18 13:21:33 +0100

    resolve app conflict

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import LoginPage from './pages/security/LoginPage';
-import RegisterPage from './pages/security/Register';
-import './App.css';
-import { fetchRecipes, fetchRecipe } from './calls/sqlite/recipe';
-import { fetchUser, fetchUsers } from './calls/sqlite/user';
-import HomePage from './pages/HomePage';
-import { RouterProvider } from 'react-router-dom';
-import ReactDOM from 'react-dom';
-import { router } from './Router/router';
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { router } from "./Router/router";
+
+import React, { useState, useEffect } from "react";
+import LoginPage from "./pages/security/LoginPage";
+import { fetchRecipes, fetchRecipe } from "./calls/sqlite/recipe";
+import { fetchUser, fetchUsers } from "./calls/sqlite/user";
+import HomePage from "./pages/HomePage";
 
 const App: React.FC = () => {
   useEffect(() => {
@@ -15,21 +15,21 @@ const App: React.FC = () => {
       .then((data) => console.log(data))
       .catch((error) => console.error(error));
 
-    fetchRecipe(1)
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
+    //fetchRecipe(1)
+    //  .then((data) => console.log(data))
+    //  .catch((error) => console.error(error));
 
     fetchUsers()
       .then((data) => console.log(data))
       .catch((error) => console.error(error));
 
-    fetchUser(1)
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
+    //fetchUser(1)
+    //  .then((data) => console.log(data))
+    //  .catch((error) => console.error(error));
   }, []);
 
   return <RouterProvider router={router} />;
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 export default App;
